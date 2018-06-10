acb144c8ed6254bf4ec7f7ac6ee0fb0de910ca13
kalyanmca13 <38880520+kalyanmca13@users.noreply.github.com>
2018-06-10 16:36:46 +0530

    Create pyth.sh

---
+++ pyth.sh
@@ -0,0 +1,13 @@
+t1=101,"kalyan",125000.00,True
+print(t1[2])
+
+idno,name,salary,status=t1
+
+print(idno,type(idno))
+print(name,type(name))
+print(salary,type(salary))
+
+count=t1.count(101)
+print(count)
+inde=t1.index(101)
+print(inde)
